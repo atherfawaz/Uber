600c79bf4b238eaac1d215b803feec5f07367173
atherfawaz <atherfawaz@users.noreply.github.com>
2019-11-25 23:43:50 +0500

    Added account attribute in person class

--- src/Person.java
+++ src/Person.java
@@ -2,73 +2,87 @@
 import java.util.*;
 
 public class Person {
-    private String name;
-    private String nationalId;
-    private Date dateOfBirth;
-    private String email;
-    private String phoneNum;
-    private Boolean isDriver;
 
-    public Person(String a, String b, Date c, String d, String e, Boolean f) {
-        name = new String(e);
-        nationalId = new String(b);
-        dateOfBirth = c;
-        email = new String(d);
-        phoneNum = new String(e);
-        isDriver = f;
-    }
+  private String name;
+  private String nationalId;
+  private Date dateOfBirth;
+  private String email;
+  private String phoneNum;
+  private Boolean isDriver;
+  private Account account;
 
-    public String getName() {
-        return name;
-    }
-    public String getNationalId() {
-        return nationalId;
-    }
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-    public Boolean getIsDriver() {
-        return isDriver;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setNationalId(String nationalId) {
-        this.nationalId = nationalId;
-    }
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-    public void setDriver(Boolean driver) {
-        isDriver = driver;
-    }
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
+  public Person(String a, String b, Date c, String d, String e, Boolean f, Account acc) {
+    name = new String(e);
+    nationalId = new String(b);
+    dateOfBirth = c;
+    email = new String(d);
+    phoneNum = new String(e);
+    isDriver = f;
+    account = acc;
+  }
 
-    public List<String> getContactInfo()
-    {
-        List<String> rList = new ArrayList<String>();
-        rList.add(this.email);
-        rList.add(this.phoneNum);
-        return rList; // Return email & phoneNum
-    }
-    public Trip getCurrentRide()
-    {
-        return null; //Implementation later
-    }
-    public void changeDetails()
-    {
-        //Implementation later
-    }
+  public String getName() {
+    return name;
+  }
+
+  public String getNationalId() {
+    return nationalId;
+  }
+
+  public Date getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPhoneNum() {
+    return phoneNum;
+  }
+
+  public Boolean getIsDriver() {
+    return isDriver;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setNationalId(String nationalId) {
+    this.nationalId = nationalId;
+  }
+
+  public void setDateOfBirth(Date dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setDriver(Boolean driver) {
+    isDriver = driver;
+  }
+
+  public void setAccount(Account acc) { account = acc; }
+
+  public void setPhoneNum(String phoneNum) {
+    this.phoneNum = phoneNum;
+  }
+
+  public List<String> getContactInfo() {
+    List<String> rList = new ArrayList<String>();
+    rList.add(this.email);
+    rList.add(this.phoneNum);
+    return rList; // Return email & phoneNum
+  }
+
+  public Trip getCurrentRide() {
+    return null; //Implementation later
+  }
+
+  public void changeDetails() {
+    //Implementation later
+  }
 }
