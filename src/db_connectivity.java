0c387ab80f37817e62d331586d8c8844e493a731
atherfawaz <atherfawaz@users.noreply.github.com>
2019-11-30 23:38:34 +0500

    Some code formatting in db_connectivity

--- src/db_connectivity.java
+++ src/db_connectivity.java
@@ -6,48 +6,39 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 
+public class db_connectivity {
 
-public class db_connectivity 
-{
-    Connection con;
-    Statement stmt;
-    
-    db_connectivity() //cons
-    {
-        try
-        {
-             String s = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=temp1";
-             con=DriverManager.getConnection(s,"new_user1","123");
+  Connection con;
+  Statement stmt;
 
+  db_connectivity() //cons
+  {
+    try {
+      String s = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=temp1";
+      con = DriverManager.getConnection(s, "new_user1", "123");
 
-            stmt = con.createStatement(); 
-            
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
+      stmt = con.createStatement();
+
+    } catch (Exception e) {
+      System.out.println(e);
     }
-    void displayAll()
-    {
-        
-        try
-        {
-            ResultSet rs=stmt.executeQuery("select * from my_table");
-             while(rs.next())
-             {
-                 
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2));
-             }
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
+  }
+
+  void displayAll() {
+
+    try {
+      ResultSet rs = stmt.executeQuery("select * from my_table");
+      while (rs.next()) {
+
+        System.out.println(rs.getInt(1) + "  " + rs.getString(2));
+      }
+    } catch (Exception e) {
+      System.out.println(e);
     }
-    public static void main(String[] args)
-    {
-    	db_connectivity d1 = new db_connectivity();
-    	d1.displayAll();
-    }
+  }
+
+  public static void main(String[] args) {
+    db_connectivity d1 = new db_connectivity();
+    d1.displayAll();
+  }
 }
