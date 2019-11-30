0b53f5fcef5de174012b7397a87f7a7b1e224d01
atherfawaz <atherfawaz@users.noreply.github.com>
2019-11-30 10:21:34 +0500

    - Made recruitStaff a public static function in UberStaff. It was previously written as a normal function in Uber.java.

--- src/Uber.java
+++ src/Uber.java
@@ -3,21 +3,15 @@ import java.util.List;
 
 public class Uber {
 
-  public static List<UberStaff> recruitStaff(int staffcount) {
-    List<UberStaff> ls = new ArrayList<>();
-    for (int i = 0; i < staffcount; i++) {
-      ls.add(new UberStaff());
-    }
-    return ls;
-  }
-
   public static void main(String[] args) {
+
     //parameters
     int STAFFCOUNT = 5;
     int HIGHESTRATED = 5;
+
     //initial setup
     LeaderBoard leaderBoard = LeaderBoard.getInstance();
-    List<UberStaff> uberstaff = recruitStaff(STAFFCOUNT);
+    List<UberStaff> uberstaff = UberStaff.recruitStaff(STAFFCOUNT);
 
     System.out.println("Welcome to Uber");
   }
