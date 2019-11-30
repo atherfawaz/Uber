42dcdc3103ec4eadbf547f2dba3b900e14858bde
atherfawaz <atherfawaz@users.noreply.github.com>
2019-11-30 18:23:54 +0500

    Made signup functions in automate.

--- src/Passenger.java
+++ src/Passenger.java
@@ -4,92 +4,90 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.time.*;
 import java.text.*;
-public class Passenger extends Person
-{
-    private List<Trip> trips;
-    public Passenger(String a, String b, String c, String d, String e, Boolean f, Account account)
-    {
-        super(a,b,c,d,e,f,account);
-    }
-   public double getExpenditure()
-    {
-        return trips.get(trips.size() - 1).getTotalCost();
-    }
-    public Boolean creditAccount(Double Amount)
-    {
-        account.addCredit(Amount);
-        return true;
-    }
-    public void initiatePayment(String pType, Double amount)
-    {
-        if (pType.equalsIgnoreCase("cash"))
-        {
-            System.out.println("You have successfully paid the driver.");
-            return;
-        }
-        makePayment(amount);
-    }
-    public Boolean rateDriver(Integer r)
-    {
-        trips.get(trips.size() -1).setRating(r);
-        return true;
-    }
-    public Boolean requestCancellation() throws ParseException {
-        Trip lastTrip = trips.get(trips.size() - 1);
-        String scheduleTime = lastTrip.getTimeForSchedule();
-        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-        Date start = format.parse(scheduleTime);
-        Date end = format.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
 
-        if (end.getTime() - start.getTime() < 20* 10000)
-            return true;
-        else
-            return false;
-    }
+public class Passenger extends Person {
 
-    public Boolean makePayment(Double amount)
-    {
-        if (account.debitAccount(amount))
-        {
-            System.out.println("You have successfully paid the driver");
-            return true;
-        }
-        return false;
+  private List<Trip> trips;
+
+  public Passenger(String a, String b, String c, String d, String e, Boolean f, Account account) {
+    super(a, b, c, d, e, f, account);
+  }
+
+  Passenger(Person passed) {
+    super(passed);
+    trips = new ArrayList<>();
+  }
+  public double getExpenditure() {
+    return trips.get(trips.size() - 1).getTotalCost();
+  }
+
+  public Boolean creditAccount(Double Amount) {
+    account.addCredit(Amount);
+    return true;
+  }
+
+  public void initiatePayment(String pType, Double amount) {
+    if (pType.equalsIgnoreCase("cash")) {
+      System.out.println("You have successfully paid the driver.");
+      return;
     }
-    public void requestAssistance()
-    {
-        System.out.println("Requesting assistance from the uber staff assigned.");
-        trips.get(trips.size()-1).helpPassenger();
+    makePayment(amount);
+  }
+
+  public Boolean rateDriver(Integer r) {
+    trips.get(trips.size() - 1).setRating(r);
+    return true;
+  }
+
+  public Boolean requestCancellation() throws ParseException {
+    Trip lastTrip = trips.get(trips.size() - 1);
+    String scheduleTime = lastTrip.getTimeForSchedule();
+    SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+    Date start = format.parse(scheduleTime);
+    Date end = format.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+
+    if (end.getTime() - start.getTime() < 20 * 10000) {
+      return true;
+    } else {
+      return false;
     }
-    public void addRide(Trip trip)
-    {
-        trips.add(trip);
+  }
+
+  public Boolean makePayment(Double amount) {
+    if (account.debitAccount(amount)) {
+      System.out.println("You have successfully paid the driver");
+      return true;
     }
-    public void displayRides()
-    {
-        for(int t = trips.size(); t>=0 ; t--)
-        {
-            System.out.println("Trip " + t+1 + " was conducted on " + trips.get(t).getDateTime());
-            System.out.println("It began at " + trips.get(t).getStartingPoint() + " and ended at " + trips.get(t).getDestination() + ".");
-            System.out.println("The total money you the ride cost the passenger was " + trips.get(t).getTotalCost() + "\n");
-        }
+    return false;
+  }
+
+  public void requestAssistance() {
+    System.out.println("Requesting assistance from the uber staff assigned.");
+    trips.get(trips.size() - 1).helpPassenger();
+  }
+
+  public void addRide(Trip trip) {
+    trips.add(trip);
+  }
+
+  public void displayRides() {
+    for (int t = trips.size(); t >= 0; t--) {
+      System.out.println("Trip " + t + 1 + " was conducted on " + trips.get(t).getDateTime());
+      System.out.println(
+          "It began at " + trips.get(t).getStartingPoint() + " and ended at " + trips.get(t)
+              .getDestination() + ".");
+      System.out.println(
+          "The total money you the ride cost the passenger was " + trips.get(t).getTotalCost()
+              + "\n");
     }
-    public Trip getCurrentRide()
-    {
-        return trips.get(trips.size() - 1);
-    }
-    public void callARide()
-    {
-        double baseFair = 100;
-        double tripDistance = (Math.random() * ((30 - 5) + 1)) + 1;
-        double totalCost = baseFair + tripDistance * 25;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your starting point: ");
-        String startingPoint = input.nextLine();
-        System.out.println("Enter your destination: ");
-        String destination = input.nextLine();
-        String currTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
-        Trip temp = new Trip(startingPoint,destination,null,null,null,this,currTime,totalCost);
-        //random gen?
-    }
+  }
+
+  public Trip getCurrentRide() {
+    return trips.get(trips.size() - 1);
+  }
+
+  public void callARide() {
+    Trip temp = new Trip();
+    //random gen?
+  }
 }
